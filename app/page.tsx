345749a246ad816918b65cd4d9e6800bcea0ca35
Gabriel Revolti <124897816+gabrielrevolti@users.noreply.github.com>
2025-06-01 22:20:00 -0300

    new Project

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="">
+      <h1 className="text-2xl">Teste</h1>
+      <Button>Teste</Button>
+    </div>
+  );
+}
